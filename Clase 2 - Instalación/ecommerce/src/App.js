6d10e7858fb7476bd7d5eecc61ff8c78307cf39a
spancavil <soancavil@gmail.com>
2022-10-13 12:11:36 -0300

    Desarrollo clase 2

---
+++ Clase 2 - Instalación/ecommerce/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Comisión 40275 - Proyecto de la clase 2
+        </p>
+        <a
+          className="App-link"
+          href="https://plataforma.coderhouse.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coderhouse
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- Clase 2 - Instalación/spreadDestructuring.js
+++ Clase 2 - Instalación/spreadDestructuring.js
@@ -1,7 +1,46 @@
 //Destructuring de objetos
+const persona = 
+{
+    nombre: "Bautista",
+    apellido: "Lombardo",
+    edad: 20,
+    vacunadoCovid: true
+}
+
+console.log(persona.apellido)
+
+const {apellido} = persona
+
+console.log(apellido)
 
 //Destructuring de arrays
 
-//Spread operator
+const frutas = ["Manzana", "Frutilla", "Limón", "Ahuacate", "Bergamota", "Kiwi"]
+
+console.log(frutas[0])
+
+const [fruta1, fruta2, ...restoDeFrutas] = frutas
+
+console.log(fruta1, fruta2)
+
+console.log(restoDeFrutas);
+
+//Spread operator (...)
+const persona2 = {...persona, tier: "diamanco"}
+
+console.log(persona2);
+
+const frutasCopia = [...frutas]
+
+const frutasConUnaFrutaAdicional = [...frutas, "Melocoton"];
+
+console.log(frutasConUnaFrutaAdicional);
 
 //Función map, find
+
+const frutasEnParagraph = frutas.map(fruta => `<p>${fruta}</p>`)
+
+console.log(frutasEnParagraph);
+
+const frutaAhuacate = frutas.find(fruta => fruta === "Aguacate");
+console.log(frutaAhuacate);
