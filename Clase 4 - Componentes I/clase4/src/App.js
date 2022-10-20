e941d54093bab932279d3aaedca9008252ec286c
spancavil <soancavil@gmail.com>
2022-10-20 11:59:14 -0300

    Desarrollo clase 4

--- Clase 4 - Componentes I/clase4/src/App.js
+++ Clase 4 - Componentes I/clase4/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
+
+  const myNfts = ["nft bonito1", "nft feo 2"]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar message = {"Hola, cómo estás?"} nfts={myNfts}/>
     </div>
   );
 }
