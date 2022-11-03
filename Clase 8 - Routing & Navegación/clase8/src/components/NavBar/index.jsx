b032a2f6323718bcee41e5af75421cad3d668f7c
spancavil <soancavil@gmail.com>
2022-11-03 06:36:20 -0300

    Template clase 8

---
+++ Clase 8 - Routing & Navegación/clase8/src/components/NavBar/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import CartWidget from "../CartWidget";
+import './styles.css';
+
+const NavBar = () => {
+
+    return (
+        <ul>
+            <li>
+                <a href="#home">Home</a>
+            </li>
+            <li>
+                <a href="#news">News</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <CartWidget />
+        </ul>
+    );
+};
+
+export default NavBar;
